299147274a6aad04f4bcd33514a16057782dc8d7
koresuniku <koresuniku@gmail.com>
2018-02-26 02:29:41 +0200

    Initial commit

---
+++ app/src/main/java/com/koresuniku/nuclearbutton/FirebaseHelper.kt
@@ -0,0 +1,36 @@
+package com.koresuniku.nuclearbutton
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import io.reactivex.Single
+
+/**
+ * Created by koresuniku on 2/26/18.
+ */
+
+class FirebaseHelper {
+    private val LOG_TAG = FirebaseHelper::class.java.simpleName
+
+    companion object {
+        const val LAST_VERSION_UPDATE = "last_version_update"
+    }
+
+    fun getLatestFromFirebaseDB(): Single<String> {
+        return Single.create({
+            val database = FirebaseDatabase.getInstance().reference
+
+            Log.d(LOG_TAG, "setting listener")
+            database.child(LAST_VERSION_UPDATE).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError?) {}
+
+                override fun onDataChange(snapshot: DataSnapshot?) {
+                    val value = snapshot?.getValue(String::class.java) ?: "Хуй"
+                    Log.d(LOG_TAG, "value: $value")
+                }
+            })
+        })
+    }
+}
---
+++ app/src/main/java/com/koresuniku/nuclearbutton/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.koresuniku.nuclearbutton
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.FirebaseApp
+import io.reactivex.schedulers.Schedulers
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+import java.util.Arrays.asList
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+
+    private lateinit var theButton: Button
+    private lateinit var firebaseHelper: FirebaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        theButton = findViewById(R.id.the_button)
+
+
+        firebaseHelper = FirebaseHelper()
+        //firebaseHelper.getLatestFromFirebaseDB().subscribeOn(Schedulers.newThread()).subscribe()
+
+        val providers = Arrays.asList(AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+            }
+        }
+    }
+}
